0bb553318632fa99f6d4f82fb9c6df87a3402c01
Pawelusze <pavlomalanich@gmail.com>
2025-07-29 21:36:20 +0200

    v3

--- Simple-Case/src/main/java/pl/polardev/scase/CasePlugin.java
+++ Simple-Case/src/main/java/pl/polardev/scase/CasePlugin.java
@@ -1,33 +1,65 @@
 package pl.polardev.scase;
 
 import org.bukkit.plugin.java.JavaPlugin;
-import pl.polardev.scase.commands.AdminCaseCommand;
-import pl.polardev.scase.listeners.CrateListener;
-import pl.polardev.scase.listeners.GUIListener;
-import pl.polardev.scase.managers.CrateManager;
+import pl.polardev.scase.command.AdminCaseCommand;
+import pl.polardev.scase.listener.CrateListener;
+import pl.polardev.scase.listener.GUIListener;
+import pl.polardev.scase.manager.AnimationManager;
+import pl.polardev.scase.manager.CrateManager;
+import pl.polardev.scase.manager.RateLimitManager;
 
-public final class CasePlugin extends JavaPlugin {
-
+public class CasePlugin extends JavaPlugin {
     private CrateManager crateManager;
+    private AnimationManager animationManager;
+    private RateLimitManager rateLimitManager;
+    private GUIListener guiListener;
 
     @Override
     public void onEnable() {
+        // Initialize managers
         this.crateManager = new CrateManager(this);
+        this.animationManager = new AnimationManager(this);
+        this.rateLimitManager = new RateLimitManager(this);
+        this.guiListener = new GUIListener(this);
 
-        getCommand("admincase").setExecutor(new AdminCaseCommand(this));
+        AdminCaseCommand adminCommand = new AdminCaseCommand(this);
+        getCommand("admincase").setExecutor(adminCommand);
+        getCommand("admincase").setTabCompleter(adminCommand);
 
         getServer().getPluginManager().registerEvents(new CrateListener(this), this);
-        getServer().getPluginManager().registerEvents(new GUIListener(this), this);
+        getServer().getPluginManager().registerEvents(guiListener, this);
+
+        getLogger().info("Simple-Case plugin enabled successfully!");
+        getLogger().info("Enterprise-level optimizations active - ready for 1000+ players");
     }
 
     @Override
     public void onDisable() {
-        if (crateManager != null) {
-            crateManager.shutdown();
+        if (rateLimitManager != null) {
+            rateLimitManager.shutdown();
         }
+        if (animationManager != null) {
+            animationManager.shutdown();
+        }
+        if (crateManager != null) {
+            crateManager.saveAllCrates();
+        }
+        getLogger().info("Simple-Case plugin disabled successfully!");
     }
 
     public CrateManager getCrateManager() {
         return crateManager;
+    }
+
+    public AnimationManager getAnimationManager() {
+        return animationManager;
+    }
+
+    public RateLimitManager getRateLimitManager() {
+        return rateLimitManager;
+    }
+
+    public GUIListener getGUIListener() {
+        return guiListener;
     }
 }
